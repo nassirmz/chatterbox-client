3335e94a69b702052c7db83a5f1165fc2a502a52
nassir <nassirmz@gmail.com>
2016-03-14 18:22:16 -0700

    Add html escaping function

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,52 +3,65 @@ var app = {
   server: 'https://api.parse.com/1/classes/chatterbox'
 };
 
-app.init = function() {
- $("#main").on("click", ".username", app.addFriend);
- $("#send").on("submit", ".submit", app.handleSubmit);
-};
+  app.escape = function (str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-app.send = function(message) {
- $.ajax({
-  url: this.server,
-  type: "POST",
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log("chatterbox: Message sent. Data: ", data);
-  },
-  error: function (data) {
-    console.log("chatterbox: Failed to send message. Error: ", data);
-  }
- })
-};
 
-app.fetch = function() {
-  $.ajax({
+  app.init = function() {
+   $("#main").on("click", ".username", app.addFriend);
+   $("#send").on("submit", ".submit", app.handleSubmit);
+  };
+
+  app.send = function(message) {
+   $.ajax({
     url: this.server,
-    type: "GET",
-    success: function(response) {
-      console.log(response);
+    type: "POST",
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log("chatterbox: Message sent. Data: ", data);
+    },
+    error: function (data) {
+      console.log("chatterbox: Failed to send message. Error: ", data);
     }
-  })
-};
-app.clearMessages = function() {
-  $("#chats").empty();
-};
+   })
+  };
 
-app.addMessage = function(message) {
-  $("#chats").append("<p><button class='username'>" + message.username + "</button><span>" + message.text + "</span><span>" + message.roomname + "</span></p>");
+  app.fetch = function() {
+    $.ajax({
+      url: this.server,
+      type: "GET",
+      success: function(data) {
+        _.each(data.results, function(message) {
+          app.escape(message.text);
+          app.escape(message.username);
+          app.escape(message.roomname);
+          app.addMessage(message);
+        })
+      }
+    })
+  };
 
-};
+  app.clearMessages = function() {
+    $("#chats").empty();
+  };
 
-app.addRoom = function(str) {
-  $("#roomSelect").append("<p>" + str + "</p>");
-}
+  app.addMessage = function(message) {
+    $("#chats").append("<p><button class='username'>" + message.username + "</button><span>" + message.text + "</span><span>" + message.roomname + "</span></p>");
 
-app.addFriend = function() {
-  
-}
+  };
 
-app.handleSubmit = function() {
+  app.addRoom = function(str) {
+    $("#roomSelect").append("<p>" + str + "</p>");
+  }
 
-}
+  app.addFriend = function() {
+    
+  }
+
+  app.handleSubmit = function() {
+
+  }
